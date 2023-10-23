e8ee1774f694f98b775bbca7c416d6fcb9693611
thisis-arman <armanhossen1549@gmail.com>
2023-10-23 09:36:36 +0600

    js drum kits done

--- Javascript Drum kits/script.js
+++ Javascript Drum kits/script.js
@@ -1,8 +1,22 @@
 console.log('from script file of drum kits')
 
 window.addEventListener("keydown",function(e){
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`
-    )
-    console.log(audio)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio){
+        return;
+    }
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add('playing')
     
 })
+function remoteTransition(e) {
+    if((e.propertyName !== 'transform')){
+        return;
+    }
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=> key.addEventListener('transitionend',remoteTransition))
